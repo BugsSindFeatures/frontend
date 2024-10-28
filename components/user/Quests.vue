8c464cfc875d11b41fe73d2ca92b7dcb96299421
BugsSindFeatures <Julian.Kausch@outlook.de>
2024-10-28 18:08:30 +0100

    working on quests 02

--- components/user/Quests.vue
+++ components/user/Quests.vue
@@ -1,11 +1,33 @@
 <template>
   <section>
     <SectionTitle sub :heading="header.heading" :body="header.body" class="!m-0" />
+    <article
+      class="flex items-center p-2 bg-warning-light text-warning rounded-md md:rounded-lg"
+    >
+      <p>+ 5</p>
+      <img
+        src="/images/coin.png"
+        :alt="t('AltAttributes.Morphcoin')"
+        class="w-8 h-8 object-contain"
+      />
+    </article>
+    <p v-if="lecturesDone < 5">
+      {{ t("Body.SolveFiveQuests") }}
+    </p>
+    <p v-else-if="lecturesDone === 5">
+      {{ t("Body.SolvedFiveQuests") }}
+    </p>
 
+    <!-- progress bar -->
     <div class="progress">
-      <div class="bar" :key="newValue" :style="{ width: `${newValue}%` }"></div>
-      <button :key="new" @click="calcValue">Click Me!{{ newValue }}</button>
+      <div
+        class="bar progress-bar h-1 w-full rounded items-center"
+        :key="progressbar_progress"
+        :style="{ width: `${progressbar_progress}%` }"
+      />
     </div>
+
+    <p v-if="lecturesDone === 5">{{ t("Body.SeeYouTomorrow") }}</p>
   </section>
 </template>
 
@@ -15,26 +37,25 @@ import { useI18n } from "vue-i18n";
 
 export default defineComponent({
   setup() {
+    const { t } = useI18n();
     const header = reactive({
-      heading: "Tägliche Quests",
+      heading: "Headings.DailyQuests",
       body: "",
     });
 
-    enum value {
-      null = 1,
-      eins = 20,
-      zwei = 40,
-      drei = 60,
-      vier = 80,
-      fuenf = 100,
+    var lecturesDone = 4;
+    var progressbar_progress = 0;
+
+    if (lecturesDone === 0) {
+      progressbar_progress = 1;
     }
-    var newValue = value.zwei;
-    function calcValue(event: any) {
-      newValue = value.drei;
-      console.log(newValue);
+    if (lecturesDone > 0) {
+      progressbar_progress = lecturesDone * 20;
     }
-    return { newValue, calcValue, header };
+
+    return { progressbar_progress, header, lecturesDone, t };
   },
+  //TODO: change backgroundcolor of progressbar to gold if lecturesDone = 5 using watch!
 });
 </script>
 
